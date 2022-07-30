79103ac378b24f42e0ddfbdfb0a1ae4b66df1431
P Luong <p-luong@live.co.uk>
2022-07-30 15:32:59 +0100

    added classes to paragraphs

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,31 @@ var update = function(){
 } 
 setInterval(update, 1000);
 
+var blocks = $(".time");
+var blocks2 = $(".container").children().eq().children("#time").html();
+
+/* var blockLength = $(".container").children();
+for (var i = 0; i < blockLength.length; i++) {
+    var blocks = $(".container").children().eq(i).children("#time").html();
+    var time = blocks;
+    console.log(time);
+} */
+
+console.log(blocks);
+console.log(blocks2);
+
+// past, present, future change colours 
+
+/* $.each(abilities, function(i, ability) {
+    abilityEl.append("<div>" + ability + "</div>");
+})
+
+if 
+
+$("textarea").addClass("past");
+$("textarea").addClass("present");
+$("textarea").addClass("future"); */
+
 function a(){
     localStorage.setItem('hour-14', 'text value')
 }
