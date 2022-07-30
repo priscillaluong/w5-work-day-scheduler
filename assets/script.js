2c882857b8f0bb6095a00270aa418478c59bc9c3
P Luong <p-luong@live.co.uk>
2022-07-30 14:31:48 +0100

    added setinterval method to update time dynamically

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
-var now = moment();
-$("#currentDay").text(now.format("dddd, Do MMMM YYYY, h:mm:ss a"));
-
 // moment.js to dynamically update seconds
 
-
+var update = function(){
+    var now = moment();
+    $("#currentDay").text(now.format("dddd, Do MMMM YYYY, h:mm:ss a"));
+} 
+setInterval(update, 1000);
 
 function a(){
     localStorage.setItem('hour-14', 'text value')
